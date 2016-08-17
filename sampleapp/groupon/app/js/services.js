d28b988804ad23a1f5f501f9e8b3a187c60bf2f9
duangrudee.pramualwuttiron <duangrudee.pramualwuttiron@thomsonreuters.com>
2016-08-17 11:34:07 +0700

    Delete not working demo

--- sampleapp/groupon/app/js/services.js
+++
@@ -1,8 +0,0 @@
-angular.module('greeter.services', [])
-
-  .factory('Web3Service', function() {
-      
-        var Web3 = require('web3');
-        var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:9000"));
-        return web3;    
-  });
