58ab88a95a2273a3ecd812327a0ff262edae9623
Duangrudee.Pramualwuttiron <duangrudee.pramualwuttiron@thomsonreuters.com>
2016-10-20 12:04:06 +0700

    Update Configuration

--- PrivateBlockSetup/runnode1_unlock.bat
+++ PrivateBlockSetup/runnode1_unlock.bat
@@ -1,2 +1,2 @@
-geth --maxpeers 3 --nodiscover --port "30303" --rpc --rpcport "9000" --rpccorsdomain "*" --datadir "privateblock" --networkid 1234  --unlock "0x5766f42ea98fcf21baeee2cd727491945a3661e2" --password "D:\Ethereum\ethereumdemo\bakupscript\unlockpass1" --nat=none console 
+geth --maxpeers 3 --nodiscover --port "30303" --rpc --rpcport "9000" --rpccorsdomain "*" --datadir "privateblock" --networkid 1234  --verbosity 6 --nat=none --unlock 0 --password password.txt console 
 
